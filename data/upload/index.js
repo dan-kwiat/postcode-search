a69dd47011f539c7dcbc4d090c90cdc667c46173
dan-kwiat <dan.kwiat@gmail.com>
2020-01-27 13:16:20 +0000

    feat(data): add method for creating id -> name map from large CSV

--- data/upload/index.js
+++ data/upload/index.js
@@ -6,12 +6,13 @@ const { bulkIndex, createIndex } = require('../lib/elastic')
 const postcodeDocParser = require('./postcode-doc-parser')
 const postcodeMappings = require('./postcode-mappings')
 
-const NUM_DOCS_ESTIMATE = 2636604
+const NUM_POSTCODES_ESTIMATE = 2636604
+const NUM_LSOAS_ESTIMATE = 42621
 
 async function csvToElastic({ filePath, batchSize, indexName, docParser }) {
   try {
     const progressBar = getProgressBar('Indexing Documents')
-    progressBar.start(NUM_DOCS_ESTIMATE, 0)
+    progressBar.start(NUM_POSTCODES_ESTIMATE, 0)
 
     if (!filePath) {
       throw new Error('Missing CSV file path')
@@ -37,6 +38,41 @@ async function csvToElastic({ filePath, batchSize, indexName, docParser }) {
   }
 }
 
+async function csvToDict({ filePath, batchSize }) {
+  try {
+    const progressBar = getProgressBar('CSV to dictionary')
+    progressBar.start(NUM_LSOAS_ESTIMATE, 0)
+
+    if (!filePath) {
+      throw new Error('Missing CSV file path')
+    }
+
+    if (!batchSize) {
+      throw new Error('Missing bulk index batch size')
+    }
+
+    let lsoas = {}
+    const batchHandler = (docs, counter) => {
+      progressBar.update(counter)
+      docs.forEach((x, index) => {
+        lsoas[x['LSOA11CD']] = x['LSOA11NM']
+      })
+      return Promise.resolve()
+    }
+
+    // pausing stream is not necessary but yolo
+    const totalCount = await streamCsv({ filePath, batchSize, batchHandler })
+
+    progressBar.update(totalCount)
+    progressBar.stop()
+    log.info(`Successfully loaded ${totalCount} rows to dict`)
+    return lsoas
+  } catch(e) {
+    log.error(`Failed to load rows to dict`)
+    throw e
+  }
+}
+
 async function esIndex() {
   try {
     await createIndex({
@@ -44,8 +80,13 @@ async function esIndex() {
       settings: {},
       mappings: postcodeMappings,
     })
+    // const lsoas = await csvToDict({
+    //   filePath: process.env.NSPL_LSOAS_CSV,
+    //   batchSize: 1000,//process.env.ELASTIC_BULK_BATCH_SIZE,
+    // })
+    // log.info('values length', Object.values(lsoas).length)
     await csvToElastic({
-      filePath: process.env.NSPL_CSV,
+      filePath: process.env.NSPL_POSTCODES_CSV,
       indexName: process.env.ELASTIC_POSTCODES_INDEX,
       batchSize: process.env.ELASTIC_BULK_BATCH_SIZE,
       docParser: postcodeDocParser,
